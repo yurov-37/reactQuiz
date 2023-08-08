33a923d17bbc5f9b27dde616c9c71eec15f69144
yurov-37 <yurov37@gmail.com>
2023-08-08 17:45:46 +0300

    handling new answers

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,16 @@ import { Main } from './Main';
 import { Loader } from './Loader';
 import { Error } from './Error';
 import { StartScreen } from './StartScreen';
+import { Question } from './Question';
 
 const initialState = {
   questions: [],
 
   //'loading','error','ready','active','finished'
   status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 const reducer = (state, action) => {
@@ -18,13 +22,28 @@ const reducer = (state, action) => {
       return { ...state, questions: action.payload, status: 'ready' };
     case 'dataFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error('Action is unknown');
   }
 };
 
 export const App = () => {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -41,7 +60,16 @@ export const App = () => {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
